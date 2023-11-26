8b18a3c8603f3c3629f1a596ed4043aaa299c726
Tran Quang Huy <107989088+tranquanghuy-09@users.noreply.github.com>
2023-11-26 08:04:00 +0700

    init

---
+++ WWW-JAVA-Week10/src/test/java/vn/edu/iuh/fit/wwwjavaweek10/WwwJavaWeek10ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.wwwjavaweek10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwwJavaWeek10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
